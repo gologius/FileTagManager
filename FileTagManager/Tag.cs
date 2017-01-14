7ca7d4e0f2df4ade944e3c517f25649441f23c7a
Nakata <kojinktganbaoru2012@gmail.com>
2017-01-14 18:49:58 +0900

    add tag class

---
+++ FileTagManager/Tag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTagManager
+{
+    class Tag
+    {
+        string name;
+        List<string> replaceTexts; //置換対象の文字列リスト
+        List<string> replacesTexts; //置換後の文字列リスト
+
+        //ファイルからタグデータを読み込む
+        public static void readTagFile(string filepath)
+        {
+
+        }
+
+        //ファイルにタグ情報を書き出す
+        public static void writeTagFile(string filepath)
+        {
+
+        }
+    }
+}
