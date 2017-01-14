15e23fa1c053a072898e7c6513c2cdb42a510ab4
Nakata <kojinktganbaoru2012@gmail.com>
2017-01-14 21:03:20 +0900

    add tagfile IO

--- FileTagManager/Tag.cs
+++ FileTagManager/Tag.cs
@@ -9,19 +9,36 @@ namespace FileTagManager
     class Tag
     {
         string name;
-        List<string> replaceTexts; //置換対象の文字列リスト
-        List<string> replacesTexts; //置換後の文字列リスト
+        List<string> replaceTexts = new List<string>(); //置換対象の文字列リスト
+        List<string> replacesTexts = new List<string>(); //置換後の文字列リスト
+
+    }
+
+    class TagList
+    {
+        List<Tag> tags = new List<Tag>();
 
         //ファイルからタグデータを読み込む
-        public static void readTagFile(string filepath)
+        public void readTagListFile(string filepath)
         {
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(TagList));
 
+            System.IO.StreamReader sr = new System.IO.StreamReader(filepath, new System.Text.UTF8Encoding(false));
+            TagList readObj = (TagList)serializer.Deserialize(sr);
+            sr.Close();
+
+            this.tags = readObj.tags; //コピー
         }
 
         //ファイルにタグ情報を書き出す
-        public static void writeTagFile(string filepath)
+        public void writeTagListFile(string filepath)
         {
-
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(TagList));
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(
+                filepath, false, new System.Text.UTF8Encoding(false));
+            serializer.Serialize(sw, this);
+            sw.Close();
         }
+
     }
 }
