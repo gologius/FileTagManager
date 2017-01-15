83809121fe89df306865d8ff6fe85c2a7e93094a
Nakata <kojinktganbaoru2012@gmail.com>
2017-01-15 19:25:50 +0900

    add edit tag function

--- FileTagManager/TagConfig.Designer.cs
+++ FileTagManager/TagConfig.Designer.cs
@@ -35,17 +35,23 @@
             this.textText = new System.Windows.Forms.TextBox();
             this.extractButton = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.replaceTextsView = new System.Windows.Forms.DataGridView();
             this.replaceText = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.replaced = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.tagListView = new System.Windows.Forms.DataGridView();
             this.tagName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.addTagButton = new System.Windows.Forms.Button();
+            this.deleteTagButton = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.replaceTextsView)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tagListView)).BeginInit();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
@@ -112,13 +118,23 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.label1);
             this.groupBox3.Controls.Add(this.replaceTextsView);
-            this.groupBox3.Location = new System.Drawing.Point(182, 13);
+            this.groupBox3.Location = new System.Drawing.Point(182, 67);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(526, 311);
+            this.groupBox3.Size = new System.Drawing.Size(526, 257);
             this.groupBox3.TabIndex = 19;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "文字置換";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 12);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "%sss% : 置換対象の文字列";
             // 
             // replaceTextsView
             // 
@@ -127,24 +143,28 @@
             this.replaceTextsView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.replaceText,
             this.replaced});
-            this.replaceTextsView.Location = new System.Drawing.Point(8, 19);
+            this.replaceTextsView.Location = new System.Drawing.Point(8, 40);
             this.replaceTextsView.Name = "replaceTextsView";
             this.replaceTextsView.RowTemplate.Height = 21;
-            this.replaceTextsView.Size = new System.Drawing.Size(512, 286);
+            this.replaceTextsView.Size = new System.Drawing.Size(512, 211);
             this.replaceTextsView.TabIndex = 22;
             // 
             // replaceText
             // 
             this.replaceText.HeaderText = "置換前の文字列";
             this.replaceText.Name = "replaceText";
+            this.replaceText.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
             // replaced
             // 
             this.replaced.HeaderText = "置換後の文字列";
             this.replaced.Name = "replaced";
+            this.replaced.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.deleteTagButton);
+            this.groupBox1.Controls.Add(this.addTagButton);
             this.groupBox1.Controls.Add(this.tagListView);
             this.groupBox1.Location = new System.Drawing.Point(12, 13);
             this.groupBox1.Name = "groupBox1";
@@ -155,20 +175,63 @@
             // 
             // tagListView
             // 
+            this.tagListView.AllowUserToAddRows = false;
+            this.tagListView.AllowUserToDeleteRows = false;
             this.tagListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.tagListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.tagListView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.tagName});
             this.tagListView.Location = new System.Drawing.Point(6, 19);
+            this.tagListView.MultiSelect = false;
             this.tagListView.Name = "tagListView";
             this.tagListView.RowTemplate.Height = 21;
-            this.tagListView.Size = new System.Drawing.Size(152, 286);
+            this.tagListView.Size = new System.Drawing.Size(152, 256);
             this.tagListView.TabIndex = 16;
+            this.tagListView.SelectionChanged += new System.EventHandler(this.tagListView_SelectionChanged);
             // 
             // tagName
             // 
             this.tagName.HeaderText = "タグ名";
             this.tagName.Name = "tagName";
+            this.tagName.ReadOnly = true;
+            this.tagName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Location = new System.Drawing.Point(182, 13);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(526, 48);
+            this.groupBox4.TabIndex = 21;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "文字抽出 (正規表現)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(514, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // addTagButton
+            // 
+            this.addTagButton.Location = new System.Drawing.Point(6, 281);
+            this.addTagButton.Name = "addTagButton";
+            this.addTagButton.Size = new System.Drawing.Size(75, 23);
+            this.addTagButton.TabIndex = 17;
+            this.addTagButton.Text = "タグ追加";
+            this.addTagButton.UseVisualStyleBackColor = true;
+            this.addTagButton.Click += new System.EventHandler(this.addTagButton_Click);
+            // 
+            // deleteTagButton
+            // 
+            this.deleteTagButton.Location = new System.Drawing.Point(82, 281);
+            this.deleteTagButton.Name = "deleteTagButton";
+            this.deleteTagButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteTagButton.TabIndex = 18;
+            this.deleteTagButton.Text = "タグ削除";
+            this.deleteTagButton.UseVisualStyleBackColor = true;
+            this.deleteTagButton.Click += new System.EventHandler(this.deleteTagButton_Click);
             // 
             // TagConfig
             // 
@@ -176,6 +239,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(715, 437);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -185,9 +249,12 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.replaceTextsView)).EndInit();
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.tagListView)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -203,9 +270,14 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DataGridView tagListView;
-        private System.Windows.Forms.DataGridViewTextBoxColumn tagName;
         private System.Windows.Forms.DataGridView replaceTextsView;
         private System.Windows.Forms.DataGridViewTextBoxColumn replaceText;
         private System.Windows.Forms.DataGridViewTextBoxColumn replaced;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tagName;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button deleteTagButton;
+        private System.Windows.Forms.Button addTagButton;
     }
 }
--- FileTagManager/TagConfig.cs
+++ FileTagManager/TagConfig.cs
@@ -14,13 +14,19 @@ namespace FileTagManager
     {
         private string tagFilePath = @"taglist.xml"; //todo configへ
         private const int TAG_NAME = 0;
+        private const int REPLACE_NAME = 0;
+        private const int REPLACED_NAME = 1;
 
+        private TagList tagList;
+       
         public TagConfig()
         {
             InitializeComponent();
 
+            tagList = new TagList(tagFilePath); //ファイルからデータを読み込む
+            
             updateTagListView();
-            updateReplaceView();
+            updateReplaceView(getSelectDataGridRowIndex(tagListView));
         }
 
         //表示の更新
@@ -28,7 +34,6 @@ namespace FileTagManager
         {
             tagListView.Rows.Clear(); //全部クリア
 
-            TagList tagList = new TagList(tagFilePath); //ファイルからデータを読み込む
             foreach (var tag in tagList.tags)
             {
                 tagListView.Rows.Add(tag.name);
@@ -36,14 +41,26 @@ namespace FileTagManager
         }
 
         //表示の更新
-        private void updateReplaceView()
+        private void updateReplaceView(int index)
         {
+            if (index < 0 || index <= tagList.tags.Count)
+                return;
 
+            replaceTextsView.Rows.Clear(); //全部クリア
+
+            Tag tag = tagList.tags[index];
+            for (int i = 0; i < tag.replaceTexts.Count; i++)
+            {
+                replaceTextsView.Rows.Add(
+                    tag.replaceTexts[i],
+                    tag.replacedTexts[i]);
+            }
         }
 
         //編集終了時
         private void TagConfig_FormClosed(object sender, FormClosedEventArgs e)
         {
+            /*
             TagList tagList = new TagList(); //ここにデータを挿入する
 
             //タグ名の読み取りと挿入
@@ -55,16 +72,95 @@ namespace FileTagManager
                         tagList.tags.Add(new Tag(cell.Value.ToString()));
                 }
             }
-
+            */
             tagList.writeTagListFile(tagFilePath);
         }
 
         private void debugMsgBox(string str)
         {
             DialogResult result = MessageBox.Show(
-                str, 
+                str,
                 "確認",
                 MessageBoxButtons.OK);
         }
+
+        private int getSelectDataGridRowIndex(DataGridView view)
+        {
+            int select_index = -1;
+
+            //選択しているtagのindexを取り出す
+            //memo プロパティのMultiSelectをFalseにしておくべき
+            foreach (var row in view.SelectedRows.Cast<DataGridViewRow>())
+            {
+                select_index = row.Index;
+                break; //一つしか使わないので．特にMultiSelect=Falseにした場合
+            }
+
+            return select_index;
+        }
+
+        //##############################################################################################################
+
+        private void tagListView_SelectionChanged(object sender, EventArgs e)
+        {
+            //変更前に選択していたtagのreplaceTextsViewの内容を保存する
+
+
+            int select_index = getSelectDataGridRowIndex(tagListView);
+            if (select_index != -1)
+            {
+                Tag tag = tagList.tags[select_index];
+                tag.replaceTexts.Clear(); //初期化
+                tag.replacedTexts.Clear();
+                foreach (var row in replaceTextsView.Rows.Cast<DataGridViewRow>())
+                {
+                    string replace_text = "";
+                    string replaced_text = "";
+                    if (row.Cells[REPLACE_NAME].Value != null)
+                        replace_text = row.Cells[REPLACE_NAME].Value.ToString();
+                    if (row.Cells[REPLACED_NAME].Value != null)
+                        replaced_text = row.Cells[REPLACED_NAME].Value.ToString();
+
+                    tag.replaceTexts.Add(replace_text);
+                    tag.replacedTexts.Add(replaced_text);
+                }
+            }
+
+            //表示の更新
+            updateReplaceView(select_index);
+        }
+
+        private void addTagButton_Click(object sender, EventArgs e)
+        {
+            string tag_name = AddTagForm.showConfirmForm();
+            if (tag_name.Equals(""))
+                return;
+        
+            
+            tagList.tags.Add(new Tag(tag_name));
+            updateTagListView();
+
+        }
+
+        private void deleteTagButton_Click(object sender, EventArgs e)
+        {
+            //選択している行を抽出
+            int select_index = getSelectDataGridRowIndex(tagListView);
+            if (select_index == -1)
+                return;
+
+            //確認，削除，更新
+            DialogResult result = MessageBox.Show(
+              "タグを削除します",
+              "タグ削除の確認",
+              MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                tagList.tags.RemoveAt(select_index);
+                updateTagListView();
+            }
+        }
+
+       
     }
 }
