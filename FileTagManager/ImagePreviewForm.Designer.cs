70d1dd5ebb44918294e997931b822ef47e9e8703
Nakata <kojinktganbaoru2012@gmail.com>
2018-12-15 18:09:25 +0900

    update image viewer ファイル名とタグを表示できるようにした

--- FileTagManager/ImagePreviewForm.Designer.cs
+++ FileTagManager/ImagePreviewForm.Designer.cs
@@ -30,7 +30,16 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.messageLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.filenameView = new System.Windows.Forms.DataGridView();
+            this.filename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.author = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.extension = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filenameView)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -38,9 +47,9 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(13, 31);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 106);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(645, 866);
+            this.pictureBox1.Size = new System.Drawing.Size(795, 791);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -49,23 +58,99 @@
             // messageLabel
             // 
             this.messageLabel.AutoSize = true;
-            this.messageLabel.ForeColor = System.Drawing.Color.Red;
-            this.messageLabel.Location = new System.Drawing.Point(13, 13);
+            this.messageLabel.ForeColor = System.Drawing.Color.Black;
+            this.messageLabel.Location = new System.Drawing.Point(12, 9);
             this.messageLabel.Name = "messageLabel";
-            this.messageLabel.Size = new System.Drawing.Size(0, 12);
+            this.messageLabel.Size = new System.Drawing.Size(78, 12);
             this.messageLabel.TabIndex = 1;
+            this.messageLabel.Text = "message label";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(732, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "反映する";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "前の本へ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(95, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "次の本へ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // filenameView
+            // 
+            this.filenameView.AllowUserToAddRows = false;
+            this.filenameView.AllowUserToDeleteRows = false;
+            this.filenameView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.filenameView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.filename,
+            this.author,
+            this.title,
+            this.extension});
+            this.filenameView.Location = new System.Drawing.Point(12, 24);
+            this.filenameView.Name = "filenameView";
+            this.filenameView.RowHeadersVisible = false;
+            this.filenameView.RowTemplate.Height = 21;
+            this.filenameView.Size = new System.Drawing.Size(714, 47);
+            this.filenameView.TabIndex = 6;
+            // 
+            // filename
+            // 
+            this.filename.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.filename.HeaderText = "filename";
+            this.filename.Name = "filename";
+            this.filename.ReadOnly = true;
+            // 
+            // author
+            // 
+            this.author.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.author.HeaderText = "author";
+            this.author.Name = "author";
+            // 
+            // title
+            // 
+            this.title.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.title.HeaderText = "title";
+            this.title.Name = "title";
+            // 
+            // extension
+            // 
+            this.extension.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.extension.HeaderText = "extension";
+            this.extension.Name = "extension";
             // 
             // ImagePreviewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 909);
+            this.ClientSize = new System.Drawing.Size(820, 909);
+            this.Controls.Add(this.filenameView);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.messageLabel);
             this.Controls.Add(this.pictureBox1);
             this.Name = "ImagePreviewForm";
             this.Text = "Preview";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ImagePreviewForm_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filenameView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,6 +160,13 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label messageLabel;
-
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView filenameView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filename;
+        private System.Windows.Forms.DataGridViewTextBoxColumn author;
+        private System.Windows.Forms.DataGridViewTextBoxColumn title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn extension;
     }
 }
